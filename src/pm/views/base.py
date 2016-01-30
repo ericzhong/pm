d5afe7875b418dc18a5190e0ad3319d2cc19a2f2
Eric Zhong <ericiszhongwenjia@qq.com>
2016-01-30 10:23:12 +0800

    fixbug: url without next for admin_required

--- src/pm/views/base.py
+++ src/pm/views/base.py
@@ -11,7 +11,8 @@ class homepage(View):
 def admin_required(function=None, login_url=None):
     actual_decorator = user_passes_test(
         lambda u: u.is_superuser,
-        login_url='/'               # can not use reverse() in urlconf
+        login_url='/',               # can not use reverse() in urlconf
+        redirect_field_name=None
     )
     if function:
         return actual_decorator(function)
